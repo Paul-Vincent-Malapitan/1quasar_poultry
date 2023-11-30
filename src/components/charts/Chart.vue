216ca95d34647d4691a96cf6c6ba9e41eaf117c2
paulvinzent <manalopv@gmail.com>
2023-12-01 05:23:09 +0800

    charts

---
+++ src/components/charts/Chart.vue
@@ -0,0 +1,41 @@
+<!-- src/components/ChartComponent.vue -->
+
+<template>
+  <div>
+    <canvas ref="myChart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  mounted() {
+    this.renderChart();
+  },
+  methods: {
+    renderChart() {
+      const ctx = this.$refs.myChart.getContext('2d');
+      new Chart(ctx, {
+        type: 'line', // Change to 'line' for a line chart
+        data: this.chartData,
+        options: this.chartOptions,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here if needed */
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,14 +4,16 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
+
       { path: '/user', component: () => import('pages/Admin/CreateUser.vue') },
-      { path: '/dashboard', component: () => import('pages/Admin/Dashboard.vue') },
+      { path: '/', component: () => import('pages/Admin/Dashboard.vue') },
       { path: '/delete', component: () => import('pages/Admin/Delete.vue') },
       { path: '/update', component: () => import('pages/Admin/Update.vue') },
       { path: '/branch_list', component: () => import('pages/Admin/BranchList.vue') },
       { path: '/add_branch', component: () => import('pages/Admin/AddBranch.vue') },
       { path: '/edit_branch', component: () => import('pages/Admin/EditBranch.vue') },
+      { path: '/chart', component: () => import('pages/Admin/Chart.vue') },
+
 
     ]
   },
